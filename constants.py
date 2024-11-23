90f38c10f616d33e02008848c4ce34dfa05ddb7c
Lukas Rennhofer <132039008+LL-Program@users.noreply.github.com>
2024-11-23 20:24:11 +0100

    Added V.0.0.2 Source Code

---
+++ constants.py
@@ -0,0 +1,31 @@
+#INTERPRETER
+VERSION = "0.0.2"
+#Dtypes - DTY 1-2
+SPEX_TYPES_ARR = []
+SPEX_TYPES_DICT = [
+    'const', # TYPE 1 - DTY2 ID 0 1
+    'NoneType' # TYPE 2 - DTY2 ID 1 2
+                   ]
+SPEX_MASS_STRUCTURES = []
+#TOKENS
+SPEX_TOKEN_SYMBOLS = '[]{}'
+SPEX_KEYWORDS = [
+    'var',
+    'mass',
+    'include',
+    'typedef', #TODO
+    'struct', #TODO
+    'func'
+]
+#Dtypes
+SPEX_DTYPES = [
+    "int",
+    "string",
+    "float",
+    "bool",
+    "char", #TODO
+    "coordinate", # TODO
+    "array", # TODO
+    "matrix", #TODO
+    'triple' #TODO
+]
